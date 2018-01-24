c2ea77c399e59b9ce636ccce31032eb56b888baf
Hunter <10795440@my.uvu.edu>
2018-01-24 09:01:43 -0700

    aaaa

--- Unity/Number Guesser/Assets/Scripts/GuesserScript.cs
+++ Unity/Number Guesser/Assets/Scripts/GuesserScript.cs
@@ -4,13 +4,58 @@ using UnityEngine;
 
 public class GuesserScript : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-        print("Our game has started!");
-	}
+    public int max;
+    public int min;
+    public int guess;
+
+    // Use this for initialization
+    private void Start () {
+
+        max = Random.Range(10, 1000);
+        print("Welcome to Number Guesser!");
+        print("Pick a number between " + min + " and " + max);
+
+        //Is the value GUESS
+        NextGuess();
+        //Instructions - Push these buttons
+        print("Up Arrow = Higher, Down Arrow = Lower, Enter = Correct");
+        max++;
+    }
+
+    void NextGuess()
+    {
+        guess = Random.Range(min, max);
+        print("Is the number " + guess + "?");
+        if (min >= max)
+        {
+            print("Computer Wins");
+            Application.Quit();
+        }
+    }
 	
 	// Update is called once per frame
-	void Update () {
-		
+    void Update () {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            print("Up Arrow Pressed");
+            min = guess+1;
+            NextGuess();
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            print("Down Arrow Pressed");
+            max = guess;
+            NextGuess();
+        }
+        else if (Input.GetKeyDown(KeyCode.Return))
+        {
+            print("Return Button Pressed");
+            print("Computer Wins");
+            Application.Quit();
+        }
+        else if (Input.anyKeyDown)
+        {
+            print("Incorrect Input");            
+        }        
 	}
 }
