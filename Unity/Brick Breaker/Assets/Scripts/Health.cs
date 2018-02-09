26738b341fbf018509d87ce68aa05fc5acb136ae
Hunter <10795440@my.uvu.edu>
2018-02-09 10:45:16 -0700

    x

---
+++ Unity/Brick Breaker/Assets/Scripts/Health.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour {
+
+    public int health;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        health--;
+        //if our health gets to zero
+        //then destroy this object
+
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Unity/Brick Breaker/Assets/Scripts/MovePaddle.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePaddle : MonoBehaviour {
+
+    public float moveSpeed = 5;
+    private Rigidbody2D rigi;
+
+    private void Awake()
+    {
+        rigi = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        rigi.velocity = new Vector3(moveSpeed, 0, 0);        
+    }
+
+    private void OnCollsionEnter2D(Collision2D coll)
+    {
+        if (tag == "Right" || tag == "Left")
+            rigi.velocity *= -1;
+    }
+}
