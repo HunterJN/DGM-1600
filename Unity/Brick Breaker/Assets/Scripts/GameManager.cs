38649cbfaa54016aa63c46c74ac6e7d22c5e71ce
Hunter <10795440@my.uvu.edu>
2018-02-16 10:16:29 -0700

    added game over screen

---
+++ Unity/Brick Breaker/Assets/Scripts/GameManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+    public static GameManager instance = null;
+
+    // Use this for initialization
+    void Awake()
+    {
+        //Singleton
+        if (instance == null)
+        {                                       //if instance is not assaigned
+            instance = this;                    //the assign istance to this object
+        }
+        else if (instance != this)
+        {
+            Destroy(this.gameObject);           //then destroy this object
+        }
+
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    public void LoadLevel(string level)
+    {
+        SceneManager.LoadScene(level);
+    }
+
+    public void QuitApplication()
+    {
+        Application.Quit();
+    }
+}
---
+++ Unity/Brick Breaker/Assets/Scripts/SpawnIn.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnIn : MonoBehaviour {
+
+    public Sprite newSprite;
+
+    void Awake()
+    {
+        Invoke("DoSomething", 1);
+        /*gameObject.GetComponent<SpriteRenderer>().sprite = newSprite;
+        Vector3 spawnPosition = new Vector3(0, 0, 0);*/        
+    }
+
+    void DoSomething()
+    {
+        gameObject.GetComponent<SpriteRenderer>().sprite = newSprite;
+        Vector3 spawnPosition = new Vector3(0, gameObject.transform.position.y, 0);
+        gameObject.transform.position = spawnPosition;
+        BoxCollider2D sc = gameObject.AddComponent(typeof(BoxCollider2D)) as BoxCollider2D;
+    }
+}
