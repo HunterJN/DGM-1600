d30803374214e68a5aeb74f3513dd5789af3bc83
Hunter <10795440@my.uvu.edu>
2018-03-05 09:48:52 -0700

    Created walk animations
    Currently not working

--- Unity/Final/Assets/Scripts/PlayerMovement.cs
+++ Unity/Final/Assets/Scripts/PlayerMovement.cs
@@ -2,14 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(CapsuleCollider2D))]
 public class PlayerMovement : MonoBehaviour {
 
     private Rigidbody2D rigid;
     public float speed;
+    private Animator anim;
+    private SpriteRenderer rend;
 
 	// Use this for initialization
 	void Start () {
         rigid = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        rend = GetComponent<SpriteRenderer>();
 	}
 	
 	// Update is called once per frame
@@ -17,8 +23,18 @@ public class PlayerMovement : MonoBehaviour {
         //check for button pushes
 
         rigid.AddForce(new Vector2(Input.GetAxis("Horizontal") * speed, 0), ForceMode2D.Force);
-        rigid.AddForce(new Vector2(0, Input.GetAxis("Vertical") * speed), ForceMode2D.Force);
-
+        //rigid.AddForce(new Vector2(0, Input.GetAxis("Vertical") * speed), ForceMode2D.Force);
+        anim.SetFloat("HorizontalMove", Input.GetAxisRaw("Horizontal"));
+        if (Input.GetAxisRaw("Horizontal") < -0.1f)
+        {
+            //flip the sprite renderer
+            rend.flipX = true;
+        }
+        else
+        {
+            //do not flip
+            rend.flipX = false;
+        }
 		
 	}
 }
