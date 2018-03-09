27bc06abefa3fa72be7a4a05597a08f286310a13
Hunter <10795440@my.uvu.edu>
2018-03-09 10:14:17 -0700

    Auto stash before merge of "master" and "origin/master"

---
+++ Unity/Final Game/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(PolygonCollider2D))]
+
+public class EnemyMovement : MonoBehaviour {
+
+    private Rigidbody2D rigid;
+    public float speed;
+    public Transform Player;
+
+    // Use this for initialization
+    void Start () {
+        rigid = GetComponent<Rigidbody2D>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        //rigid.AddForce(new Vector2(Input.GetAxis("Horizontal") * speed, 0), ForceMode2D.Force);
+        //rigid.AddForce(new Vector2(0, Input.GetAxis("Vertical") * speed), ForceMode2D.Force);
+        if (Player == true)
+        {
+            float step = speed * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, Player.position, step);
+        }
+        else
+        {
+            SceneManager.LoadScene("Game Over");
+        }
+    }
+}
---
+++ Unity/Final Game/Assets/Scripts/GameManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+    public static GameManager instance = null;
+
+    // Use this for initialization
+    void Awake()
+    {
+        /*var height = 2 * Camera.main.orthographicSize;
+        var width = height * Camera.main.aspect;
+        print(height);
+        print(width);*/
+
+        //Singleton
+        if (instance == null)
+        {                                       //if instance is not assaigned
+            instance = this;                    //the assign istance to this object
+        }
+        else if (instance != this)
+        {
+            Destroy(this.gameObject);           //then destroy this object
+        }
+
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    public void LoadLevel(string level)
+    {
+        SceneManager.LoadScene(level);
+    }
+
+    public void LoadNextLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitApplication()
+    {
+        Debug.Log("Quit Game");
+        Application.Quit();
+    }
+}
